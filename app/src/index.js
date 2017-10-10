6b4f6a4977d0354377bb94af6b073266af93758d
chate <chatethakhun.jack@hotmail.com>
2017-10-10 22:45:46 +0700

    createreact with node

---
+++ app/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './components/App/App';
+import registerServiceWorker from './registerServiceWorker';
+
+import { browserHistory } from 'react-router';
+
+import Routes from './routes';
+
+ReactDOM.render(<Routes history={browserHistory} />, document.getElementById('root'));
+registerServiceWorker();
