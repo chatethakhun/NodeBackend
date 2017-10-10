ec79711c1cae175ec4a4d5b22ff7781d3d41b13d
chate <chatethakhun.jack@hotmail.com>
2017-10-10 23:49:44 +0700

    add navbar

--- app/src/routes.js
+++ app/src/routes.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import { Router, Route } from 'react-router';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router'
 
 import App from './components/App/App';
+import Home from './components/Home/Home';
 
 
 const Routes = (props) => (
-  <Router {...props}>
-    <Route path="/" component={App} />
+  <Router history={browserHistory}>
+    <Route path='/'
+           component={App}>
+      <IndexRoute component={Home} />
+
+    </Route>
   </Router>
+
 );
 
 export default Routes;
