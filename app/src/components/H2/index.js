3ffa6367ba3b04962cc9bad12964bf4653ca7db1
jack <chatethakhun.jack@hotmail.com>
2017-10-25 18:19:33 +0700

    add content in nav bar

---
+++ app/src/components/H2/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const H2 = props => {
+    return(
+        <p>{props.value}</p>
+    )
+}
--- app/src/containers/HomePage.js
+++ app/src/containers/HomePage.js
@@ -5,6 +5,7 @@ import React from 'react'
 
 import { NavBar, FlexContainer } from '../theme/'
 import {H1} from '../components/H1/'
+import {H2} from '../components/H2/'
 
 
 class HomePage extends React.Component {
@@ -13,6 +14,7 @@ class HomePage extends React.Component {
             <NavBar>
                 <FlexContainer>
                     <H1 value={'MY COLLECTION GUNDAM'}></H1>
+                    <H2 value={'My facebook'} />
                 </FlexContainer>
             </NavBar>
         )
